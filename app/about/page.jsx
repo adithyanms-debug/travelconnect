7096913e29f35153ee34062354bb6dd63d56440f
adithyanms-debug <adithyanms1007@gmail.com>
2026-07-22 16:55:05 +0000

    Added my name

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
         <>
-            <h1>This is my first next js assignment</h1>
+            <h1>This is my first next js assignment. I am Adithyan from CSE-A</h1>
         </>
     );
 }
