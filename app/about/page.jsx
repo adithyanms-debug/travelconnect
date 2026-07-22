6a2051ff6a1ace5e922dc42798beaa39f45a8db9
adithyanms-debug <adithyanms1007@gmail.com>
2026-07-22 16:57:17 +0000

    Made some changes in about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
         <>
-            <h1>This is my first next js assignment. I am Adithyan from CSE-A</h1>
+            <h1>This is my first next js assignment. I am Adithyan M S from S5 CSE-A</h1>
         </>
     );
 }
